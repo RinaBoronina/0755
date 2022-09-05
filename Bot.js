f8b651586031bd63d7c78660ab717d4928699498
RinaBoronina <111557018+RinaBoronina@users.noreply.github.com>
2022-09-05 09:24:23 +0300

    Внесены изменения набора по буквам ключевых фраз

--- Bot.js
+++ Bot.js
@@ -1,34 +1,36 @@
-// ==UserScript==
-// @name         GoogleBot
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!Bot for Google
-// @author       Boronina Karina
-// @match        https://www.google.com/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
-// ==/UserScript==
-
-
 let links = document.links;
-let btnK = document.getElementsByName('btnK')[0];
-let keyWords = ["вывод произвольных полей wordpress", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress"];
-let keyWord = keyWords[getRandom(0, keyWords.length)];
+let btnK = document.getElementsByName("btnK")[0];
+let keywords = ["Взаимодействие PHP и MySQL", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress"];
+let keyword = keywords[getRandom(0, keywords.length)];
+
+let googleInput = document.getElementsByName("q")[0];
 
 
 if (btnK !== undefined) {
-    document.getElementsByName('q')[0].value = keyWord;
+    let i = 0;
+
+    let timerId = setInterval(() => {
+    googleInput.value += keyword[i];
+    i++;
+    if (i == keyword.length) {
+    clearInterval(timerId);
     btnK.click();
+}
+}, 500);
+
 } else {
-for( let i = 0; i <links.length; i++) {
+
+  for (let i=0; i<links.length; i++) {
     if (links[i].href.indexOf("napli.ru") !== -1) {
-        console.log("Нашел строку!" + links[i]);
-        let link = links[i];
-        link.click();
-        break;
+      console.log("Нашел строку" + links[i]);
+      let link = links[i];
+      link.click();
+      break;
     }
   }
- }
-   function getRandom(min,max) {
-    return Math.floor(Math.random()*(max - min) + min);
 }
+
+function getRandom(min, max) {
+  return Math.floor(Math.random()*(max - min) + min);
+}
+
