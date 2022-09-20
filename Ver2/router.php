2fe61aaeeb85d31127cbc1a37fdea234a50de2a8
RinaBoronina <111557018+RinaBoronina@users.noreply.github.com>
2022-09-20 11:59:07 +0300

    Добавлена работа с ЛК

--- Ver2/router.php
+++ Ver2/router.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 $url = explode('/', $_SERVER['REQUEST_URI']);
 require_once("php/db.php");
 require_once("php/classes/User.php");
@@ -19,6 +19,10 @@ if ($url[1] == "contact") {
     echo User::addUser($_POST["name"], $_POST["lastname"], $_POST["email"], $_POST["pass"]);
 } else if ($url[1] == "authUser") {
     echo User::authUser($_POST["email"], $_POST["pass"]);
+} else if ($url[1] == "getUser") {
+    echo User::getUser($_SESSION["id"]);
+} else if ($url[1] == "getUsers") {
+    echo User::getUsers();
 } else {
     $content = file_get_contents("pages/index.php");
 }
